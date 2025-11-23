edff7f18da6736c5e980ccc6ab7c70c81082cb8e
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-11-23 00:00:15 -0300

    tabuada

---
+++ Desafio04.py
@@ -0,0 +1,10 @@
+num= int(input('Digite aqui um número para ver a tabuada de 1 a 10:'))
+for tabuada in range(1,11):
+    multiplicacao= num*tabuada
+    print(f'{num} x {tabuada} = {multiplicacao}')
+print('Fim do programa')
+
+'''
+Refaça o Desafio 009, mostrando a tabuada de um número que o usuário escolher, só que 
+agora utilizando um laço for.
+'''
