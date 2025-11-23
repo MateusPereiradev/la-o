f79d05af4d626bd4c06933c3fd71b9a249636468
mateuspereiradev <dev.mateuspereira@gmail.com>
2025-11-23 11:06:02 -0300

    Fim_programa

--- Desafio05.py
+++ Desafio05.py
@@ -4,6 +4,7 @@ for pares in range(1,7):
     if num%2==0:
         soma+=num
 print(f'A soma dos números que são só pares são {soma}')
+print('Fim do programa')
 
 
 '''Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem
